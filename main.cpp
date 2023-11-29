c1b29e10c30df34d17643f24dce8ae03282a5f45
Nicole Futoryansky <nfutor2005@gmail.com>
2023-11-29 15:24:38 -0500

    Added ui

--- main.cpp
+++ main.cpp
@@ -9,13 +9,18 @@
 #define SCREEN_WIDTH 319
 #define SCREEN_HEIGHT 239
 
+//enum for the type of the ball
+enum BallType {Blue, Red, Cue, Eight };
+
 class Ball {
     private:
         float posX, posY, velX, velY;
         int radius = 5;
+        BallType type;
     public:
-        Ball(float px, float py, float vx, float vy);
-        void render();
+        Ball(float px, float py, float vx, float vy, BallType t);
+        void render(); //
+
         //getters and setters
         float getPosX();
         void setPosX(float px);
@@ -30,6 +35,7 @@ class Ball {
         //updates position
         void update();
 
+
 };
 
 class Board {
@@ -38,6 +44,9 @@ class Board {
     public:
         Board();
         void render();
+
+        //checks for collisions with the walls
+        void checkWalls();
 };
 
 //global vars
@@ -51,8 +60,8 @@ int main() {
     //local vars to main
     bool running = true;
 
-    Ball ball1 = Ball(100, 100, 1, 1);
-    Ball ball2 = Ball(200, 250, -1, -1);
+    Ball ball1 = Ball(100, 100, 1, 1, Red);
+    Ball ball2 = Ball(200, 300, -2, -1, Blue);
 
     while (running) {
         if(state == 'm'){
@@ -60,7 +69,7 @@ int main() {
         }
         else if(state == 'p'){
             //placeholder
-            LCD.WriteAt("PLAYING", 100, 20);
+            // LCD.WriteAt("PLAYING", 100, 20);
             playing(ball1, ball2);
         }
         else if(state == 't'){
@@ -148,25 +157,31 @@ void retMenu(){
 
 void playing(Ball& b1, Ball& b2) {   
 
-    float hyp = dist(b1.getPosX(), b1.getPosY(), b2.getPosX(), b2.getPosY());
+    // float hyp = dist(b1.getPosX(), b1.getPosY(), b2.getPosX(), b2.getPosY());
     
-    if (hyp < b1.getRadius()*2){
-        float tX = b2.getVelX();
-        float tY = b2.getVelY();
+    // if (hyp < b1.getRadius()*2){
+    //     float tX = b2.getVelX();
+    //     float tY = b2.getVelY();
 
-        b2.setVelX(b1.getVelX());
-        b2.setVelY(b1.getVelY());
+    //     b2.setVelX(b1.getVelX());
+    //     b2.setVelY(b1.getVelY());
 
-        b1.setVelX(tX);
-        b1.setVelY(tY);
-    }
+    //     b1.setVelX(tX);
+    //     b1.setVelY(tY);
+    // }
+
+    // b1.update();
+    // b2.update();
+
+    // LCD.Clear(BLACK);
+    // LCD.DrawCircle((int) b1.getPosX(), (int) b1.getPosY(), (int) b1.getRadius());
+    // LCD.DrawCircle((int) b2.getPosX(), (int) b2.getPosY(), (int) b2.getRadius());
     
-    b1.update();
-    b2.update();
 
-    LCD.Clear(BLACK);
-    LCD.DrawCircle((int) b1.getPosX(), (int) b1.getPosY(), (int) b1.getRadius());
-    LCD.DrawCircle((int) b2.getPosX(), (int) b2.getPosY(), (int) b2.getRadius());
+    Board board = Board();
+    board.render();
+   
+    
 }
 
 
@@ -176,11 +191,12 @@ float dist(float x, float y, float x1, float y1){
     return hyp;
 }
 
-Ball::Ball(float px, float py, float vx, float vy) {
+Ball::Ball(float px, float py, float vx, float vy, BallType t) {
     posX = px;
     posY = py;
     velX = vx;
     velY = vy;
+    type = t;
 }
 
 void Ball::update() {
@@ -188,8 +204,26 @@ void Ball::update() {
     posY+=velY;
 }
 
+//The ball draws itself
 void Ball::render() {
+    //determine which color the ball should be drawn in
+    switch (type) {
+        case Red:
+            LCD.SetFontColor(MAROON);
+            break;
+        case Blue:
+            LCD.SetFontColor(DARKBLUE);
+            break;
+        case Cue:
+            LCD.SetFontColor(WHITE);
+            break;
+        case Eight:
+            LCD.SetFontColor(BLACK);
+            break;
+    }
     LCD.FillCircle((int) posX, (int) posY, (int) radius);
+
+    LCD.SetFontColor(WHITE); //set font color back to white
 }
 
 float Ball::getPosX() {
@@ -201,7 +235,7 @@ void Ball::setPosX(float px) {
 } 
 
 float Ball::getPosY() {
-    return posX;
+    return posY;
 }
 
 void Ball::setPosY(float py) {
@@ -217,7 +251,7 @@ void Ball::setVelX(float vx) {
 } 
 
 float Ball::getVelY() {
-    return velX;
+    return velY;
 }
 
 void Ball::setVelY(float vy) {
@@ -228,13 +262,73 @@ int Ball::getRadius() {
     return radius;
 }
 
-
+//initalize
 Board::Board() {
-
+    Ball rb1 = Ball(200, 80, 0, 0, Red);
+    balls.push_back(rb1);
+    Ball rb2 = Ball(200, 95, 0, 0, Red);
+    balls.push_back(rb2);
+    Ball rb3 = Ball(200, 110, 0, 0, Red);
+    balls.push_back(rb3);
+    Ball rb4 = Ball(200, 125, 0, 0, Red);
+    balls.push_back(rb4);
+    Ball rb5 = Ball(200, 140, 0, 0, Red);
+    balls.push_back(rb5);
+    Ball rb6 = Ball(200, 165, 0, 0, Red);
+    balls.push_back(rb6);
+    Ball rb7 = Ball(200, 140, 0, 0, Red);
+    balls.push_back(rb7);
+    Ball bb1 = Ball(250, 80, 0, 0, Blue);
+    balls.push_back(bb1);
+    Ball bb2 = Ball(250, 95, 0, 0, Blue);
+    balls.push_back(bb2);
+    Ball bb3 = Ball(250, 110, 0, 0, Blue);
+    balls.push_back(bb3);
+    Ball bb4 = Ball(250, 125, 0, 0, Blue);
+    balls.push_back(bb4);
+    Ball bb5 = Ball(250, 140, 0, 0, Blue);
+    balls.push_back(bb5);
+    Ball bb6 = Ball(250, 165, 0, 0, Blue);
+    balls.push_back(bb6);
+    Ball bb7 = Ball(250, 140, 0, 0, Blue);
+    balls.push_back(bb7);
+    Ball cueBall = Ball(150, 120, 0, 0, Cue);
+    balls.push_back(cueBall);
+    Ball eightBall = Ball(150, 150, 0, 0, Eight);
+    balls.push_back(eightBall);
 }
 
+//Draws the board and all of the balls on it
 void Board::render() {
+    LCD.SetFontColor(SADDLEBROWN); //the brown border
+    LCD.FillRectangle(7, 72, 306, 156);
+    LCD.SetFontColor(DARKGREEN); //the green table
+    LCD.FillRectangle(11, 76, 298, 148);
+    LCD.SetFontColor(WHITE); //the player name labels
+    LCD.WriteAt("Player 1", 5, 5);
+    LCD.WriteAt("Player 2", 190, 5);
+    LCD.DrawRectangle(109, 4, 17, 17); //the white boxes around the player colors
+    LCD.DrawRectangle(294, 4, 17, 17);
+    LCD.SetFontColor(DARKBLUE); //the blue color box
+    LCD.FillRectangle(110, 5, 15, 15);
+    LCD.SetFontColor(MAROON); //the red color box
+    LCD.FillRectangle(295, 5, 15, 15);
+    //Draw pockets
+    LCD.SetFontColor(BLACK);
+    LCD.FillCircle(9, 74, 8);
+    LCD.FillCircle(9, 225, 8);
+    LCD.FillCircle(310, 74, 8);
+    LCD.FillCircle(310, 225, 8);
+    LCD.FillCircle(160, 74, 8);
+    LCD.FillCircle(160, 225, 8);
+
+    LCD.SetFontColor(WHITE); //set font color back to white
+
+    //render each ball
     for (int i = 0; i < balls.size(); i++) {
        balls.at(i).render();
     }
+}
+
+void Board::checkWalls() {
 }
